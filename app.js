52d2a65e4fbf24b94409ec1442ea9065434804fd
kiendt <dotrungkien6987@gmail.com>
2024-12-16 16:53:55 +0700

    update whitelist

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var whitelist = [
   `http://localhost:3001`,
   `http://192.168.1.248:3000`,
   `http://27.72.116.110:777`,
-  `http://bvdkphutho.io.vn`,
+  `http://bvdkphutho.io.vn:777`,
   `http://api.bvdkphutho.io.vn`,
 ];
 var corsOptionsDelegate = function (req, callback) {
